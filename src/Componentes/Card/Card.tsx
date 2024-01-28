bcf72daf6043c168a34df98c136846fbfd9aa57d
joao-code-web <joaogabriel9512@gmail.com>
2024-01-28 08:38:30 -0300

    deploy

--- src/Componentes/Card/Card.tsx
+++ src/Componentes/Card/Card.tsx
@@ -1,4 +1,3 @@
-import { useContext, useState } from "react"
 import { Form } from "../Layouts/Form/Form";
 import ListTransactions from "../Layouts/ListTransactions/ListTransactions";
 import Dashboard from "../Layouts/Dashboard/Dasboard";
--- src/Hooks/UseSomTransactionsValues.tsx
+++ src/Hooks/UseSomTransactionsValues.tsx
@@ -1,20 +1,30 @@
-    import axios from 'axios';
+import axios from 'axios';
 
-    export default function UseSomTransactionsValues() {
-        async function somas(setFunc) {
-            try {
-                const response = await axios.get("http://localhost:3000");
-                const data = response.data;
+import { Dispatch, SetStateAction } from "react";
 
-                const somaAll = data.reduce((acc, transaction) => acc + parseFloat(transaction.value), 0);
+interface Transaction {
+  value: string; // Ou ajuste para o tipo real do valor
+}
 
-                setFunc(somaAll);
-            } catch (error) {
-                console.error("Erro ao obter dados da API:", error);
-            }
+interface UseSomTransactionsValuesProps {
+    setFunc: Dispatch<SetStateAction<number>>;
+}
+
+export default function UseSomTransactionsValues() {
+    async function somas({ setFunc }: UseSomTransactionsValuesProps) {
+        try {
+            const response = await axios.get<Transaction[]>("http://localhost:3000");
+            const data = response.data;
+
+            const somaAll = data.reduce((acc, transaction) => acc + parseFloat(transaction.value), 0);
+
+            setFunc(somaAll);
+        } catch (error) {
+            console.error("Erro ao obter dados da API:", error);
         }
-
-        return {
-            somas
-        };
     }
+
+    return {
+        somas
+    };
+}
